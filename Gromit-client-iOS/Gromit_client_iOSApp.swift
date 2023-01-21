e71b447b39ce312a7d194b18f7e0e0410645d2d4
Juhee <viimv2000@gmail.com>
2023-01-21 19:06:09 +0900

    Initial Commit

---
+++ Gromit-client-iOS/Gromit_client_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gromit_client_iOSApp.swift
+//  Gromit-client-iOS
+//
+//  Created by juhee on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Gromit_client_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
