f96556839fb2d8dd7bc935a8477265b29937c7d6
Juhee <viimv2000@gmail.com>
2023-01-27 16:27:59 +0900

    feat: 새로 키우기 alert 창 구현

--- Gromit-client-iOS/Source/Views/FinalLevelView.swift
+++ Gromit-client-iOS/Source/Views/FinalLevelView.swift
@@ -14,22 +14,7 @@ struct FinalLevelView: View {
             
             CharacterView()
             
-            HStack {
-                Button {
-                    
-                } label: {
-                    Text("새로 키우기")
-                        .frame(width: 160, height: 34)
-                        .font(.system(size: 16))
-                        .foregroundColor(Color(.black))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 30)
-                                .stroke(Color("gray500"))
-                        )
-                }
-            }
-            .padding(EdgeInsets(top: 40, leading: 0, bottom: 0, trailing: 0))
-
+            NewCharacterButton()
         }
     }
 }
@@ -55,5 +40,36 @@ struct FinalLevelText: View {
             }
             .padding(EdgeInsets(top: 10, leading: 0, bottom: 30, trailing: 0))
         }
+    }
+}
+
+struct NewCharacterButton: View {
+    @State private var showingAlert = false
+    
+    var body: some View {
+        HStack {
+            Button {
+                self.showingAlert.toggle()
+            } label: {
+                Text("새로 키우기")
+                    .frame(width: 160, height: 34)
+                    .font(.system(size: 16))
+                    .foregroundColor(Color(.black))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(Color("gray500"))
+                    )
+            }
+            .alert(isPresented: $showingAlert) {
+                Alert(
+                    title: Text("최종 진화에 도달하셨습니다!\n새로운 캐릭터를 키워보시겠습니까?"),
+                    message: Text("기존의 캐릭터는 컬렉션에 추가됩니다."),
+                    primaryButton: .default(Text("좋아요"), action: {
+                        
+                    }),
+                    secondaryButton: .cancel(Text("아니오")))
+            }
+        }
+        .padding(EdgeInsets(top: 40, leading: 0, bottom: 0, trailing: 0))
     }
 }
