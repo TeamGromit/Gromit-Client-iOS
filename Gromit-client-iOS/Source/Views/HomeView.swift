276d1fd697a428074f456ea52da646851598e954
Juhee <viimv2000@gmail.com>
2023-01-27 14:08:08 +0900

    feat: #2 홈화면 구현중

--- Gromit-client-iOS/Source/Views/HomeView.swift
+++ Gromit-client-iOS/Source/Views/HomeView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            Image("home")
+        VStack {
+            HomeButtons()
+            
+            TodaysCommit()
+            
+            CharacterView()
+            
+            VStack {
+                HStack(spacing: 0) {
+                    Text("Lv.")
+                    Text("0")
+                    Text(" ")
+                    Text("알")
+                    Text(" ( ")
+                    Text("52")
+                    Text(" / 100 )")
+                }
+                
+                CharacterLevelBar()
+                    .frame(width: 277, height: 55)
+            }
         }
     }
 }
@@ -18,5 +37,79 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+struct HomeButtons: View {
+    var body: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Image("refresh")
+            }
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image("collection")
+            }
+        }
+    }
+}
+
+struct TodaysCommit: View {
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("오늘의 커밋")
+                .font(.system(size: 20))
+            Text("23")
+                .font(.system(size: 40))
+        }
+    }
+}
+
+struct CharacterView: View {
+    var body: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 30)
+                .fill(Color("green300"))
+                .frame(width: 277, height: 277)
+                .overlay(RoundedRectangle(cornerRadius: 30)
+                    .stroke(Color("green500"), lineWidth: 5))
+        }
+    }
+}
+
+struct CharacterLevelBar: View {
+    @State private var containerWidth: CGFloat = 0
+    @State private var step = 50
+    private let goal = 100
+    
+    var maxWidth: Double {
+        return min((277 / CGFloat(goal) * CGFloat(step)), containerWidth)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            GeometryReader { geo in
+                RoundedRectangle(cornerRadius: 60)
+                    .foregroundColor(.clear)
+                    .onAppear {
+                        containerWidth = geo.size.width
+                    }
+            }
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color(.white))
+                
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color("green500"))
+                .frame(width: maxWidth, height: 55)
+        }
+        .fixedSize(horizontal: false, vertical: true)
+        .overlay(RoundedRectangle(cornerRadius: 15)
+            .stroke(Color(.black)))
     }
 }
