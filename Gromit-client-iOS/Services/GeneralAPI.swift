0e2b4668b96f7ab430e160a65dba36142b8cc7bb
Juhee <viimv2000@gmail.com>
2023-01-30 04:25:46 +0900

    add: Services 폴더 추가

---
+++ Gromit-client-iOS/Services/GeneralAPI.swift
@@ -0,0 +1,12 @@
+//
+//  GeneralAPI.swift
+//  Gromit-client-iOS
+//
+//  Created by juhee on 2023/01/30.
+//
+
+import Foundation
+
+struct GeneralAPI {
+        static let baseURL = "https://gromit.shop/"
+}
