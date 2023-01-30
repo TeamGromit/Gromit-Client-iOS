c015db9fe21aeefbe7aad4b0fe5a6d60431b7dc2
Juhee <viimv2000@gmail.com>
2023-01-30 14:24:21 +0900

    feat: Moya 초기 세팅

---
+++ Gromit-client-iOS/Services/Entities/LoginWithAppleEntity.swift
@@ -0,0 +1,22 @@
+//
+//  LoginWithAppleModel.swift
+//  Gromit-client-iOS
+//
+//  Created by juhee on 2023/01/30.
+//
+
+import Foundation
+
+struct LoginWithAppleEntity: Codable {
+    let code: Int
+    let isSuccess: Bool
+    let message: String
+    let result: LoginWithAppleResponse
+}
+
+struct LoginWithAppleResponse: Codable {
+    // 기존 회원시
+    let accessToken, refreshToken: String?
+    // 신규 회원시
+    let email, provider: String?
+}
