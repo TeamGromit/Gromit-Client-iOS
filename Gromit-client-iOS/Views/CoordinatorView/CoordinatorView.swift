89b35a1019f866e1a2a548a3fbe0a8f230c2b38e
Kim TaeSung <fopc4131@gmail.com>
2023-02-19 23:57:17 +0900

    CoordinatorPattern Source Update

---
+++ Gromit-client-iOS/Views/CoordinatorView/CoordinatorView.swift
@@ -0,0 +1,47 @@
+//
+//  CoordinatorView.swift
+//  Gromit-client-iOS
+//
+//  Created by 김태성 on 2023/02/17.
+//
+
+import SwiftUI
+
+struct CoordinatorView: View {
+    
+    @StateObject private var coordinator = Coordinator()
+    @State private var tabSelection = 2
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.clear
+        
+    }
+    var body: some View {
+        TabView(selection: $tabSelection) {
+            ChallengeListView()
+                .tabItem{
+                    Image("challenge")
+                }.tag(1)
+            
+            HomeView()
+                .tabItem{
+                    Image("home")
+                }.tag(2)
+            
+            SettingsView()
+                .tabItem{
+                    Image("settings")
+                }.tag(3)
+        }
+        .fullScreenCover(item: $coordinator.fullScreenCover, content: { fullScreenCover in
+            coordinator.build(fullScreenCover: fullScreenCover)
+        })
+        .environmentObject(coordinator)
+        //.toolbarBackground(Color.clear, for: .tabBar)
+    }
+}
+
+struct CoordinatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoordinatorView()
+    }
+}
