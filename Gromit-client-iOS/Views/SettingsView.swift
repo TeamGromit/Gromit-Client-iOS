0f378d0962853d25ded179d7df2967225cf8d803
jinnxyoung <wlsdud7722@naver.com>
2023-03-26 02:43:57 +0900

    style: 텍스트 오타 수정

--- Gromit-client-iOS/Views/SettingsView.swift
+++ Gromit-client-iOS/Views/SettingsView.swift
@@ -14,66 +14,66 @@ struct SettingsView: View {
         
         NavigationView {
             Form {
-                    Toggle(isOn: $showingToggle) {
-                        Text(" 알람")
+                Toggle(isOn: $showingToggle) {
+                    Text("알람")
+                }
+                
+                if showingToggle {
+                    Button(action: {
+                    }) {
+                        DatePicker(
+                            "시간을 설정하세요",
+                            selection: $date,
+                            displayedComponents: [.hourAndMinute]
+                        )
                     }
-                    
-                    if showingToggle {
-                        Button(action: {
-                        }) {
-                                    DatePicker(
-                                      "시간을 설정하세요",
-                                      selection: $date,
-                                      displayedComponents: [.hourAndMinute]
-                                    )
-                                  }
-                        }
-                Link(" 이용약관", destination: URL(string: "https://www.notion.so/Gromit-Team-27c8612cb66a446b962b1fb29f8957d4")!)
+                }
+                Link("이용약관", destination: URL(string: "https://www.notion.so/Gromit-Team-27c8612cb66a446b962b1fb29f8957d4")!)
                     .foregroundColor(.black)
                 
-                Text(" 버전 정보")
+                Text("버전 정보")
                 
-                Button(" 닉네임변경") {
-                showSignInView.toggle()
-            }
-            .fullScreenCover(isPresented: $showSignInView) {
-                ChangeNameView()
-            }
+                Button("닉네임 변경") {
+                    showSignInView.toggle()
+                }
+                .fullScreenCover(isPresented: $showSignInView) {
+                    ChangeNameView()
+                }
                 .foregroundColor(.black)
                 
-                Button(" 로그아웃") {
-                          self.showingAlert.toggle()
-                      }
-                      .foregroundColor(.black)
-                      .alert(isPresented: $showingAlert) {
-                          let firstButton = Alert.Button.default(Text("OK")) {
-                              print("primary button pressed")
-                          }
-                          let secondButton = Alert.Button.cancel(Text("Cancel")) {
-                              print("secondary button pressed")
-                          }
-                          return Alert(title: Text("로그아웃 하시겠습니까?"),
-                                       primaryButton: firstButton, secondaryButton: secondButton)
-                      }
+                Button("로그아웃") {
+                    self.showingAlert.toggle()
+                }
+                .foregroundColor(.black)
+                .alert(isPresented: $showingAlert) {
+                    let firstButton = Alert.Button.default(Text("OK")) {
+                        print("primary button pressed")
+                    }
+                    let secondButton = Alert.Button.cancel(Text("Cancel")) {
+                        print("secondary button pressed")
+                    }
+                    return Alert(title: Text("로그아웃 하시겠습니까?"),
+                                 primaryButton: firstButton, secondaryButton: secondButton)
+                }
                 
-                Button(" 서비스탈퇴") {
-                          self.showingAlert2.toggle()
-                      }
-                      .foregroundColor(.black)
-                      .alert(isPresented: $showingAlert2) {
-                          let firstButton = Alert.Button.default(Text("돌아가기")) {
-                              print("primary button pressed")
-                          }
-                          let secondButton = Alert.Button.cancel(Text("탈퇴하기")) {
-                              print("secondary button pressed")
-                          }
-                          return Alert(title: Text("탈퇴를 진행할 경우 모든 정보가 삭제됩니다. 정말 탈퇴하시겠습니까?"),
-                                       primaryButton: firstButton, secondaryButton: secondButton)
-                      }
-                  }
-              }
-                        }
-                        }
+                Button("서비스 탈퇴") {
+                    self.showingAlert2.toggle()
+                }
+                .foregroundColor(.black)
+                .alert(isPresented: $showingAlert2) {
+                    let firstButton = Alert.Button.default(Text("돌아가기")) {
+                        print("primary button pressed")
+                    }
+                    let secondButton = Alert.Button.cancel(Text("탈퇴하기")) {
+                        print("secondary button pressed")
+                    }
+                    return Alert(title: Text("서비스 탈퇴를 진행할 경우,\n앱 내의 모든 정보가 삭제됩니다.탈퇴를 진행하시겠습니까?"),
+                                 primaryButton: firstButton, secondaryButton: secondButton)
+                }
+            }
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
