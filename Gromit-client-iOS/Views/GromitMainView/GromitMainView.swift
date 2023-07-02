34d3ac219ba1d8a5b0a5c79610d7c67f0f5de010
jinnxyoung <wlsdud7722@naver.com>
2023-07-02 23:13:27 +0900

    feat: SettingView 구현

--- Gromit-client-iOS/Views/GromitMainView/GromitMainView.swift
+++ Gromit-client-iOS/Views/GromitMainView/GromitMainView.swift
@@ -39,7 +39,7 @@ struct GromitMainView: View {
                     Image("home")
                 }.tag(2)
                 
-            SettingsView()
+            SettingView()
                 .tabItem{
                     Image("settings")
                 }.tag(3)
---
+++ Gromit-client-iOS/Views/SettingsView/SettingView.swift
@@ -0,0 +1,99 @@
+//
+//  SettingView.swift
+//  Gromit-client-iOS
+//
+//  Created by 김진영 on 2023/06/29.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    @State private var toggling = false
+    @State private var showingToggle = false
+    @State private var showingAlert = false
+    @State private var showingAlert2 = false
+    @State private var showingAlert3 = false
+    @State private var showSignInView = false
+    @State var date = Date()
+    
+    @EnvironmentObject private var coordinator: Coordinator
+    @StateObject private var settingsViewModel = SettingsViewModel()
+    
+    init() {
+        print("SettingView init!")
+    }
+    
+    var body: some View {
+        List {
+            Toggle(isOn: $showingToggle) {
+                Text("알람")
+            }
+            if showingToggle {
+                Button(action: {}) {
+                    DatePicker(
+                        "알람 시간 설정",
+                        selection: $date,
+                        displayedComponents: [.hourAndMinute]
+                    )
+                }
+            }
+            
+            Link("이용 약관", destination: URL(string: "https://www.notion.so/Gromit-Team-27c8612cb66a446b962b1fb29f8957d4")!)
+                .foregroundColor(.black)
+            
+            HStack {
+                Text("버전 정보")
+                Spacer()
+                Text("v0.1.0").foregroundColor(.gray)
+            }
+            
+            Button("닉네임 변경") {
+                //showSignInView.toggle()
+                coordinator.push(.settingView, page: .changeGromitUserNameView)
+            }
+            .fullScreenCover(isPresented: $showSignInView) {
+                ChangeNameView()
+            }
+            .foregroundColor(.black)
+            
+            Button("로그아웃") {
+                self.showingAlert.toggle()
+            }
+            .foregroundColor(.black)
+            .alert(isPresented: $showingAlert) {
+                let firstButton = Alert.Button.default(Text("OK")) {
+                    print("primary button pressed")
+                    settingsViewModel.removeToken()
+                    // 로그아웃 후, 로그인 페이지로 이동 후 다시 재로그인 했을 때 홈 화면으로 전환이 안 됨
+                    coordinator.rootPage = .signInView
+                }
+                let secondButton = Alert.Button.cancel(Text("Cancel")) {
+                    print("secondary button pressed")
+                }
+                return Alert(title: Text("로그아웃 하시겠습니까?"),
+                             primaryButton: firstButton, secondaryButton: secondButton)
+            }
+            
+            Button("서비스 탈퇴") {
+                self.showingAlert2.toggle()
+            }
+            .foregroundColor(.black)
+            .alert(isPresented: $showingAlert2) {
+                let firstButton = Alert.Button.default(Text("돌아가기")) {
+                    print("primary button pressed")
+                }
+                let secondButton = Alert.Button.cancel(Text("탈퇴하기")) {
+                    print("secondary button pressed")
+                }
+                return Alert(title: Text("탈퇴를 진행할 경우\n모든 정보가 삭제됩니다.\n계속 진행하시겠습니까?"),
+                             primaryButton: firstButton, secondaryButton: secondButton)
+            }
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
